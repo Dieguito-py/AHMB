66b7d9dafba8bd637bc085b66575b0317f916a27
Dieguito-py <DiegoBernardi@protonmail.com>
2023-12-29 12:54:56 -0300

    add filles

--- analysis.py
+++ analysis.py
@@ -2,33 +2,26 @@ import requests
 import matplotlib.pyplot as plt
 
 def fetch_data_from_server():
-    url = 'http://localhost/send-data' 
-    response = requests.post(url)
+    url = 'http://localhost:5000/send-data'  
+    response = requests.get(url)
+
     if response.status_code == 200:
-        data = response.text
+        data = response.text  
         return data
     else:
-        print('Falha ao buscar dados do servidor')
+        print("Falha ao buscar dados do servidor")
         return None
 
 def plot_data(data):
-    if data:
-        print('Resposta do servidor:', data)
-
-        numeric_values = [val.strip() for val in data.split(',') if val.replace('.', '', 1).isdigit()]
-        if numeric_values:
-            values = [float(val) for val in numeric_values]
-            plt.plot(values)
-            plt.title('Dados do Acelerômetro')
-            plt.xlabel('Amostras')
-            plt.ylabel('Valores')
-            plt.show()
-        else:
-            print('Nenhum dado numérico encontrado na resposta do servidor')
-    else:
-        print('Nenhum dado recebido do servidor')
-
+    x = ['AcelX', 'AcelY', 'AcelZ']
+    y = [10, 20, 15]  
+    plt.bar(x, y)
+    plt.xlabel('Eixos do Acelerômetro')
+    plt.ylabel('Valores')
+    plt.title('Dados do Acelerômetro')
+    plt.show()
 
 if __name__ == '__main__':
-    data_from_server = fetch_data_from_server()
-    plot_data(data_from_server)
+    fetched_data = fetch_data_from_server()
+    if fetched_data:
+        plot_data(fetched_data)
--- server.py
+++ server.py
@@ -1,13 +1,38 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
-@app.route('/send-data', methods=['POST'])
+x_data, y_data, z_data = [], [], []
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/data', methods=['POST'])
 def receive_data():
-    if request.method == 'POST':
-        data = request.data.decode('utf-8')
-        print("Dados recebidos:", data)
-        return "Dados recebidos com sucesso!"
+    data = request.data.decode('utf-8')
+    if data:
+        ax, ay, az = map(float, data.split(','))
+        x_data.append(ax)
+        y_data.append(ay)
+        z_data.append(az)
+        print(f"Received data: X={ax}, Y={ay}, Z={az}")
+    return 'Data received'
+
+@app.route('/plot')
+def plot_data():
+    plt.figure(figsize=(8, 6))
+    plt.plot(x_data, label='X')
+    plt.plot(y_data, label='Y')
+    plt.plot(z_data, label='Z')
+    plt.xlabel('Samples')
+    plt.ylabel('Accelerometer Values')
+    plt.legend()
+    plt.title('MPU6050 Accelerometer Data')
+    plt.savefig('static/plot.png')  # Salva o gráfico como um arquivo estático
+    plt.close()
+    return render_template('plot.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True)
