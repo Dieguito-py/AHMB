38b5a1a2a95aa0a769031370d84e84bb31beaae2
Dieguito-py <diegoBernardi@protonmail.com>
2024-03-18 10:00:05 -0300

    update

--- data_visualization.py
+++ data_visualization.py
@@ -6,15 +6,25 @@ dados = pd.read_csv("dados.csv")
 
 dados.columns = ['tempo', 'xaxle', 'yaxle', 'zaxle', 'service']
 
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(18, 6))
+
+plt.subplot(1, 3, 1)
 sns.boxplot(x='service', y='xaxle', data=dados)
 plt.xlabel('Service')
 plt.ylabel('X Axle')
-# plt.title('Boxplot de X Axle por Service')
+plt.title('X Axle')
 
-plt.figure(figsize=(10, 6))
+plt.subplot(1, 3, 2)
 sns.boxplot(x='service', y='yaxle', data=dados)
 plt.xlabel('Service')
-plt.ylabel('X Axle')
+plt.ylabel('Y Axle')
+plt.title('Y Axle')
 
+plt.subplot(1, 3, 3)
+sns.boxplot(x='service', y='zaxle', data=dados)
+plt.xlabel('Service')
+plt.ylabel('Z Axle')
+plt.title('Z Axle')
+
+plt.tight_layout()
 plt.show()
